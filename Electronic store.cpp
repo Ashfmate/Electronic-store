2b4d87599c3e3cf4edb7ff32787b22609ca7ab14
Ashmate <46440581+Ashfmate@users.noreply.github.com>
2023-05-26 11:51:52 +0300

    Cleaned up the #include mess and also constructed the Login_page object and used the function operator as one expression

--- Electronic store.cpp
+++ Electronic store.cpp
@@ -1,12 +1,8 @@
-#include <iostream>
-#include <fmt/format.h>
 #include <conio.h>
-#include <Windows.h>
 #include "Login_page.h"
 
 int main()
 {
-	Login_page log;
-	log();
+	Login_page{}();
 	while (!_kbhit());
 }
