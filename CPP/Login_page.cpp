7325affadc5ddfb6937d21042d445636adc61f76
Ashmate <46440581+Ashfmate@users.noreply.github.com>
2023-05-27 19:23:13 +0300

    Added the login page with a primitive boolean return value (later it will be an optional struct of a person)

--- CPP/Login_page.cpp
+++ CPP/Login_page.cpp
@@ -1,1 +1,101 @@
 #include "../Headers/Login_page.h"
+#include <fmt\format.h>
+#include <fmt\color.h>
+#include <conio.h>
+
+using fmt::print;
+
+bool Login_page::operator()()
+{
+	while (true)
+	{
+		system("cls");
+
+		if (leave())
+			return false;
+
+		draw();
+
+		if (!confirm())
+			continue;
+
+		if (check_user_and_pass())
+			return true;
+	}
+}
+
+void Login_page::draw()
+{
+	system("cls");
+	print(fmt::fg(fmt::color::blue), "Enter your username: ");
+	username = read();
+	print(fmt::fg(fmt::color::blue), "\nEnter your password: ");
+	password = read_hidden();
+}
+
+bool Login_page::leave()
+{
+	print(fmt::fg(fmt::color::red), "Do you want to go back? (y/n) ");
+	return _getch() == 'y';
+}
+
+bool Login_page::confirm()
+{
+	print(fmt::fg(fmt::color::red), "\nDo you confirm? (y/n) ");
+	return _getch() == 'y';
+}
+
+bool Login_page::check_user_and_pass()
+{
+	std::string path = "Profiles/" + username + ".txt";
+	std::ifstream file(path);
+	if (!file)
+		return false;
+	std::stringstream line;
+	while (getline(file, line))
+	{
+		while (line)
+		{
+			std::string word;
+			line >> word;
+			if (word == "password")
+			{
+				line >> word >> word;
+				return word == password;
+			}
+			else
+				break;
+		}
+	}
+	return false;
+}
+
+std::string Login_page::read()
+{
+	std::string result;
+	for (auto answer = _getch(); answer != 13 && answer != ' '; answer = _getch())
+	{
+		result.push_back(answer);
+		_putch(answer);
+	}
+	return result;
+}
+
+std::string Login_page::read_hidden()
+{
+	std::string result;
+	for (auto answer = _getch(); answer != 13 && answer != ' '; answer = _getch())
+	{
+		result.push_back(answer);
+		_putch('*');
+	}
+	return result;
+}
+
+std::ifstream& Login_page::getline(std::ifstream& file, std::stringstream& sstr)
+{
+	std::string line;
+	std::getline(file, line);
+	sstr.str(line);
+	return file;
+}
--- Headers/Login_page.h
+++ Headers/Login_page.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <string>
+#include <fstream>
+#include <sstream>
+
+class Login_page
+{
+public:
+	bool operator()();
+private:
+	void draw();
+	bool leave();
+	bool confirm();
+	bool check_user_and_pass();
+	std::string read();
+	std::string read_hidden();
+	std::ifstream& getline(std::ifstream& file, std::stringstream& sstr);
+private:
+	std::string username = "";
+	std::string password = "";
+	bool exit = false;
+};
