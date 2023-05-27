2ae1381332f000b2d640cccd005fc904648ce1b0
Ashmate <46440581+Ashfmate@users.noreply.github.com>
2023-05-27 11:05:40 +0300

    Propping up the program class

--- CPP/Program.cpp
+++ CPP/Program.cpp
@@ -1,12 +1,37 @@
 #include "../Headers/Program.h"
 
-
 Program::Program()
 {
-
+	funcs[Signing] = [&]()
+	{
+		Signin_page_result = Signing_page{}();
+	};
+	funcs[Login] = [&]()
+	{};
+	funcs[Register] = [&]()
+	{};
+	funcs[Quitting] = [&]()
+	{};
 }
 
 int Program::start()
 {
-    return 0;
+	return 0;
+}
+
+Program::Pages Program::sign_choice_to_Pages_converter(sign_choice input)
+{
+	switch (input)
+	{
+	case sign_choice::Login:
+		return Login;
+		break;
+	case sign_choice::Register:
+		return Register;
+		break;
+	case sign_choice::Quit:
+		return Quitting;
+		break;
+	}
+	return Signing;
 }
--- Headers/Program.h
+++ Headers/Program.h
@@ -8,7 +8,22 @@ class Program
 public:
 	Program();
 	int start();
+public:
+	enum Pages : size_t
+	{
+		Signing,
+		Login,
+		Register,
+		Quitting,
+		Count
+	};
 private:
-	std::array<std::function<void()>, size_t(Signing_page::Choices::Count)> funcs;
+	typedef Signing_page::Choices sign_choice;
+private:
+	Pages sign_choice_to_Pages_converter(sign_choice input);
+private:
+	std::array<std::function<void()>, Count> funcs;
+	bool exit_confirm = false;
+	sign_choice Signin_page_result = sign_choice::Start;
 };
 
